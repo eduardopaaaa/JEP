2b1d65709f487122a6f920f869e3f6a572eb7750
eduardopaaaa <eddy.nator@hotmail.com>
2022-08-08 08:55:25 -0600

    pushing express & PW updates

---
+++ express practice/site.js
@@ -0,0 +1,16 @@
+data = {
+    infoArray: ['Welcome to the internet home of Bungie','Developer of Destiny, Halo, Myth, Oni, and Marathon'],
+   
+    historyArray: ['Est. 1991', 'We are an American video game company based in Bellevue, Washington. The company was established by Alex Seropian, who later brought in programmer Jason Jones'],
+   
+    contactArray: ['Contact us at:', 'bunjie.net'],
+   
+    linksArray: ['Follow our links', 'www.google.com'],
+   
+    pastArray: ['Our past projects:', 'Destiny, Halo, Myth, Oni, and Marathon'],
+   
+    staffArray: ['Staff members', 'Our team']
+   }
+   
+   module.exports = data
+   
